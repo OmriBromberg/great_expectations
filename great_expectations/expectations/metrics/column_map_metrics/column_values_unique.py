52414608e23629e053dd2aed62fe824d8afb6cd9
Omri Bromberg <obbromberg@gmail.com>
2021-05-24 15:35:43 +0300

    hotfix dialect

--- great_expectations/expectations/metrics/column_map_metrics/column_values_unique.py
+++ great_expectations/expectations/metrics/column_map_metrics/column_values_unique.py
@@ -62,7 +62,7 @@ class ColumnValuesUnique(ColumnMapMetricProvider):
         # the column we will be performing the expectation on, and the query is performed against it.
         dialect = kwargs.get("_dialect", None)
         sql_engine = kwargs.get("_sqlalchemy_engine", None)
-        if sql_engine and dialect and dialect.dialect.name == "mysql":
+        if sql_engine and dialect and sql_engine.dialect.name == "mysql":
             temp_table_name = f"ge_tmp_{str(uuid.uuid4())[:8]}"
             temp_table_stmt = "CREATE TEMPORARY TABLE {new_temp_table} AS SELECT tmp.{column_name} FROM {source_table} tmp".format(
                 new_temp_table=temp_table_name,
